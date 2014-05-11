69e31622ba2fff1dff7e5ec26df1b51f2619a54a
Alle Veenstra <alle.veenstra@gmail.com>
2014-05-11 11:59:21 -0500

    improved program arguments

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ gpxcat
 
 Gpxcat is an utility for concatinating, simplifing and splitting GPX tracks.
 
+# Examples
+
+The following examples show how to use this tool.
+
 ## Remove all segments and split the tracks
 
 In this example all tracks are concatinated, individual segments removed and the track is split.
@@ -21,4 +25,22 @@ By default 500 points are kept, you can alter this using the -keep N flag.
 
 ```bash
 ./gpxcat -resample -keep 400 out/colombia/*.gpx > colombia-small.gpx
+```
+
+## Concatinate and sort
+
+Here all tracks are concatinated and sorted.
+Also the individual segments are merged into one big segment.
+
+```bash
+./gpxcat -sort out/argentina/*.gpx out/bolivia/*.gpx out/peru/*.gpx out/ecuador/*.gpx out/colombia/*.gpx > south-america.gpx
+```
+
+## Concatinate
+
+This example shows how to simply concatinate files without any modifications.
+Note that the program's arguments are important in this case.
+
+```bash
+./gpxcat south-america.gpx central-america.gpx > whole-trip.gpx
 ```
--- src/gpxcat.go
+++ src/gpxcat.go
@@ -9,19 +9,23 @@ import (
 	"time"
 )
 
-var Keep int
-var Simplify bool
+var Sort bool
 var Split bool
+var SplitFormat string
+var SplitOutput string
+var Simplify bool
 var Resample bool
-var Out string
-var Format string
+var MergeSegments bool
+var Keep int
 
 func init() {
-	flag.StringVar(&Format, "format", "2006-01-02", "the format of tracknames")
-	flag.StringVar(&Out, "out", "out", "the directory to output tracks (must exist)")
+	flag.BoolVar(&Sort, "sort", false, "sort the segments (default for merge, split & resample)")
 	flag.BoolVar(&Split, "split", false, "split the track according to a format")
+	flag.StringVar(&SplitFormat, "split-format", "2006-01-02", "the format of tracknames")
+	flag.StringVar(&SplitOutput, "split-output", "out", "the directory to output tracks (must exist)")
 	flag.BoolVar(&Simplify, "simplify", false, "simplify the track")
 	flag.BoolVar(&Resample, "resample", false, "resample the track")
+	flag.BoolVar(&MergeSegments, "merge-segments", false, "merge all segments into one bigger segment")
 	flag.IntVar(&Keep, "keep", 200, "number of points to keep")
 }
 
@@ -49,19 +53,25 @@ func main() {
 	cat.Trk.Segments = gpx.Unique(cat.Trk.Segments)
 
 	// desegmentize
-	cat.Trk.Segments = gpx.Desegmentize(cat.Trk.Segments)
+	if Sort || MergeSegments || Simplify || Resample || Split {
+		cat.Trk.Segments = gpx.Desegmentize(cat.Trk.Segments)
+	}
 
 	// parse dates && validate
-	points := cat.Trk.Segments[0].Points
-	for index := range points {
-		i_time, i_err := time.Parse("2006-01-02T15:04:05Z", points[index].Time)
-		points[index].GoTime = i_time
-		points[index].Valid = i_err == nil
-		points[index].Removed = false
+	for segment_index := range cat.Trk.Segments {
+		points := cat.Trk.Segments[segment_index].Points
+		for point_index := range points {
+			i_time, i_err := time.Parse("2006-01-02T15:04:05Z", points[point_index].Time)
+			points[point_index].GoTime = i_time
+			points[point_index].Valid = i_err == nil
+			points[point_index].Removed = false
+		}
 	}
 
 	// sort
-	sort.Sort(gpx.ByDate(cat.Trk.Segments[0].Points))
+	if Sort || MergeSegments || Simplify || Resample || Split {
+		sort.Sort(gpx.ByDate(cat.Trk.Segments[0].Points))
+	}
 
 	if Simplify {
 		gpx.Simplify(cat.Trk.Segments, Keep)
@@ -72,7 +82,7 @@ func main() {
 	}
 
 	if Split {
-		err := gpx.Split(cat, Out, Format)
+		err := gpx.Split(cat, SplitOutput, SplitFormat)
 		fatal(err)
 	} else {
 		cat.Print(os.Stdout)
